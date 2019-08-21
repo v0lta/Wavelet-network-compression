0d97c4f6ff8228e86cc3bf8ed8406f466071f184
moritz <wolter@cs.uni-bonn.de>
2019-08-21 17:17:39 +0200

    more work on PR condition.

--- wave/learn_wave.py
+++ wave/learn_wave.py
@@ -1,11 +1,12 @@
 import torch
 import numpy as np
 from wave.lowlevel import afb1d
+import ipdb
 '''A learnable filter wavelet transform in 1D.'''
 
 
 class Wave1D(torch.nn.Module):
-    def __init__(self, dec_lo, dec_hi, rec_lo, rec_hi, scales=1,
+    def __init__(self, init_wavelet, scales=1,
                  mode='zero'):
         super().__init__()
 
@@ -13,10 +14,11 @@ class Wave1D(torch.nn.Module):
             return torch.tensor(np.copy(np.array(filt_lst).ravel()[::-1]),
                                 dtype=torch.float)  # , device=x.device
 
-        self.dec_lo = to_tensor(dec_lo)
-        self.dec_hi = to_tensor(dec_hi)
-        self.rec_lo = to_tensor(rec_lo)
-        self.rec_hi = to_tensor(rec_hi)
+        self.init_wavelet = init_wavelet
+        self.dec_lo = to_tensor(init_wavelet.dec_lo)
+        self.dec_hi = to_tensor(init_wavelet.dec_hi)
+        self.rec_lo = to_tensor(init_wavelet.rec_lo)
+        self.rec_hi = to_tensor(init_wavelet.rec_hi)
 
         self.scales = scales
         self.mode = mode
@@ -41,8 +43,11 @@ class Wave1D(torch.nn.Module):
         return anti_sym
 
     def alias_cancellation_loss(self) -> torch.Tensor:
-        assert self.rec_lo.shape == self.dec_hi.shape
-        assert self.rec_hi.shape == self.dec_lo.shape
+        '''
+        Strang 105: F0(z) = H1(-z); F1(z) = -H0(-z)
+        '''
+        assert self.rec_lo.shape == self.dec_hi.shape, 'filters must have the same sizes'
+        assert self.rec_hi.shape == self.dec_lo.shape, 'filters must have the same sizes'
 
         m1 = torch.Tensor([-1])
         length = self.rec_lo.shape[0]
@@ -57,7 +62,36 @@ class Wave1D(torch.nn.Module):
         return err1 + err2
 
     def perfect_reconstruction_loss(self) -> torch.Tensor:
-        pass
+        '''
+        Strang 107:
+        Assuming alias cancellation holds:
+        P(z) = F(z)H(z)
+        Product filter P(z) + P(-z) = 2.
+        However since alias cancellation is implemented as soft constraint:
+        P_0 + P_1 = 2
+        '''
+        assert self.dec_lo.shape == self.rec_lo.shape, 'filters must have the same sizes'
+        assert self.dec_hi.shape == self.rec_hi.shape, 'filters must have the same sizes'
+        assert self.dec_lo.shape == self.dec_hi.shape, 'filters must have the same sizes'
+
+        # polynomial multiplication is convolution, compute p(z):
+        pad = self.dec_lo.shape[0]-1  # TODO: len(input/2) ?
+        p_lo = torch.nn.functional.conv1d(
+            self.dec_lo.unsqueeze(0).unsqueeze(0),
+            self.rec_lo.unsqueeze(0).unsqueeze(0),
+            padding=pad)
+
+        pad = self.dec_hi.shape[0]-1
+        p_hi = torch.nn.functional.conv1d(
+            self.dec_hi.unsqueeze(0).unsqueeze(0),
+            self.rec_hi.unsqueeze(0).unsqueeze(0),
+            padding=pad)
+
+        p_test = p_lo + p_hi
+        two_at_power_zero = torch.zeros(p_test.shape)
+        ipdb.set_trace()
+        two_at_power_zero[..., p_test.shape[-1]//2] = 2
+        return torch.sum((p_test - two_at_power_zero)*(p_test - two_at_power_zero))
 
     def orthogonal_filter_cond(self):
         '''
@@ -65,9 +99,9 @@ class Wave1D(torch.nn.Module):
         and filter banks; Page 111.
         '''
         # all filter shapes must be the same.
-        assert self.dec_lo.shape == self.dec_hi.shape
-        assert self.rec_lo.shape == self.rec_hi.shape
-        assert self.dec_lo.shape == self.rec_lo.shape
+        assert self.dec_lo.shape == self.dec_hi.shape, 'filters must have the same sizes'
+        assert self.rec_lo.shape == self.rec_hi.shape, 'filters must have the same sizes'
+        assert self.dec_lo.shape == self.rec_lo.shape, 'filters must have the same sizes'
 
         # if even:
         if self.dec_lo.shape[0] % 2 == 0:
--- wavelet_mackey.py
+++ wavelet_mackey.py
@@ -37,8 +37,14 @@ mackey_data = torch.squeeze(generator())
 # try out the pywavelets wavelet transfrom.
 mackey_data_numpy = mackey_data.detach().cpu().numpy()
 
-# wavelet = pywt.Wavelet('haar')
-wavelet = pywt.Wavelet('bior2.4')
+wavelet = pywt.Wavelet('haar')
+# wavelet = pywt.Wavelet('bior2.4')
+p_lo = np.convolve(wavelet.filter_bank[0], wavelet.filter_bank[2])
+p_hi = np.convolve(wavelet.filter_bank[1], wavelet.filter_bank[3])
+p_sum = p_lo + p_hi
+print('pr lo', np.sum(p_lo))
+print('pr hi', np.sum(p_hi))
+
 cA, cD = pywt.dwt(mackey_data_numpy[0, :], wavelet=wavelet)
 plt.plot(mackey_data_numpy[0, :])
 plt.show()
@@ -69,8 +75,7 @@ plt.show()
 reconstruction = pywave_inverse((cA_pyt, cD_pyt))
 
 # test my own code 1d 1 level.
-wave1d = Wave1D(wavelet.dec_lo, wavelet.dec_hi, wavelet.rec_lo, wavelet.rec_hi,
-                scales=1)
+wave1d = Wave1D(wavelet, scales=1)
 low, high = wave1d.analysis(mackey_data.unsqueeze(1).unsqueeze(1).cpu())
 plt.plot(high[0, 0, 0, :].detach().cpu().numpy(),
          low[0, 0, 0, :].detach().cpu().numpy())
@@ -81,11 +86,12 @@ print('high diff', np.linalg.norm(cA - high[0, 0, 0, :].detach().cpu().numpy()))
 print('done')
 
 # try out the multilevel version.
-wave1d_10 = Wave1D(wavelet.dec_lo, wavelet.dec_hi, wavelet.rec_lo, wavelet.rec_hi,
-                   scales=5)
+wave1d_10 = Wave1D(wavelet, scales=5)
 wave1d_10r = wave1d_10.analysis(mackey_data.unsqueeze(1).unsqueeze(1).cpu())
-print('alias_cancellation_error:', wave1d_10.alias_cancellation_error().numpy(), ',',
+print('alias_cancellation_error:', wave1d_10.alias_cancellation_loss().numpy(), ',',
       wavelet.name)
+print(wave1d_10.perfect_reconstruction_loss())
+
 
 for no, cp in enumerate(wave1d_10r):
     cp = cp[0, 0, 0, :].detach().numpy()
