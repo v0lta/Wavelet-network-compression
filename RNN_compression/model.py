a08e1212b083aaf41a60a38ee19a66ce2c24c0cc
moritz <wolter@cs.uni-bonn.de>
2019-12-09 14:38:22 +0100

    char model code.

---
+++ RNN_compression/model.py
@@ -0,0 +1,94 @@
+# Created by moritz (wolter@cs.uni-bonn.de) at 09/12/2019
+"""
+Based on https://github.com/locuslab/TCN/blob/master/TCN/tcn.py
+"""
+
+from torch import nn
+import sys
+sys.path.append("../../")
+import torch.nn as nn
+from torch.nn.utils import weight_norm
+
+
+class Chomp1d(nn.Module):
+    def __init__(self, chomp_size):
+        super(Chomp1d, self).__init__()
+        self.chomp_size = chomp_size
+
+    def forward(self, x):
+        return x[:, :, :-self.chomp_size].contiguous()
+
+
+class TemporalBlock(nn.Module):
+    def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
+        super(TemporalBlock, self).__init__()
+        self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp1 = Chomp1d(padding)
+        self.relu1 = nn.ReLU()
+        self.dropout1 = nn.Dropout(dropout)
+
+        self.conv2 = weight_norm(nn.Conv1d(n_outputs, n_outputs, kernel_size,
+                                           stride=stride, padding=padding, dilation=dilation))
+        self.chomp2 = Chomp1d(padding)
+        self.relu2 = nn.ReLU()
+        self.dropout2 = nn.Dropout(dropout)
+
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
+        self.relu = nn.ReLU()
+        self.init_weights()
+
+    def init_weights(self):
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+        if self.downsample is not None:
+            self.downsample.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        out = self.net(x)
+        res = x if self.downsample is None else self.downsample(x)
+        return self.relu(out + res)
+
+
+class TemporalConvNet(nn.Module):
+    def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
+        super(TemporalConvNet, self).__init__()
+        layers = []
+        num_levels = len(num_channels)
+        for i in range(num_levels):
+            dilation_size = 2 ** i
+            in_channels = num_inputs if i == 0 else num_channels[i-1]
+            out_channels = num_channels[i]
+            layers += [TemporalBlock(in_channels, out_channels, kernel_size, stride=1, dilation=dilation_size,
+                                     padding=(kernel_size-1) * dilation_size, dropout=dropout)]
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class TCN(nn.Module):
+    def __init__(self, input_size, output_size, num_channels, kernel_size=2, dropout=0.2, emb_dropout=0.2):
+        super(TCN, self).__init__()
+        self.encoder = nn.Embedding(output_size, input_size)
+        self.tcn = TemporalConvNet(input_size, num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.decoder = nn.Linear(input_size, output_size)
+        self.decoder.weight = self.encoder.weight
+        self.drop = nn.Dropout(emb_dropout)
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.encoder.weight.data.uniform_(-initrange, initrange)
+        self.decoder.bias.data.fill_(0)
+        self.decoder.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, x):
+        # input has dimension (N, L_in), and emb has dimension (N, L_in, C_in)
+        emb = self.drop(self.encoder(x))
+        y = self.tcn(emb.transpose(1, 2))
+        o = self.decoder(y.transpose(1, 2))
+        return o.contiguous()
---
+++ penn_treebank/penn_test.py
@@ -0,0 +1,230 @@
+"""
+based on https://github.com/locuslab/TCN/tree/master/TCN/char_cnn
+"""
+
+import argparse
+import torch.nn as nn
+import torch.optim as optim
+import sys
+sys.path.append("../")
+from penn_treebank.utils import *
+import time
+import math
+from RNN_compression.cells import WaveletGRU, FastFoodGRU, GRUCell
+
+import warnings
+warnings.filterwarnings("ignore")   # Suppress the RunTimeWarning on unicode
+
+
+parser = argparse.ArgumentParser(description='Sequence Modeling - Character Level Language Model')
+parser.add_argument('--batch_size', type=int, default=32, metavar='N',
+                    help='batch size (default: 32)')
+parser.add_argument('--cuda', action='store_false',
+                    help='use CUDA (default: True)')
+parser.add_argument('--clip', type=float, default=0.15,
+                    help='gradient clip, -1 means no clip (default: 0.15)')
+parser.add_argument('--epochs', type=int, default=100,
+                    help='upper epoch limit (default: 100)')
+parser.add_argument('--levels', type=int, default=3,
+                    help='# of levels (default: 3)')
+parser.add_argument('--log-interval', type=int, default=100, metavar='N',
+                    help='report interval (default: 100')
+parser.add_argument('--lr', type=float, default=4,
+                    help='initial learning rate (default: 4)')
+parser.add_argument('--emsize', type=int, default=100,
+                    help='dimension of character embeddings (default: 100)')
+parser.add_argument('--optim', type=str, default='SGD',
+                    help='optimizer to use (default: SGD)')
+parser.add_argument('--validseqlen', type=int, default=320,
+                    help='valid sequence length (default: 320)')
+parser.add_argument('--seq_len', type=int, default=400,
+                    help='total sequence length, including effective history (default: 400)')
+parser.add_argument('--seed', type=int, default=1111,
+                    help='random seed (default: 1111)')
+parser.add_argument('--dataset', type=str, default='ptb',
+                    help='dataset to use (default: ptb)')
+parser.add_argument('--cell', type=str, default='WaveGRU', help='The cell type used')
+parser.add_argument('--cell_size', type=int, default=512, help='Cell state size.')
+parser.add_argument('--compression_mode', type=str, default='state',
+                    help='Where to apply the compression layers.')
+args = parser.parse_args()
+
+# Set the random seed manually for reproducibility.
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    if not args.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+
+print(args)
+file, file_len, valfile, valfile_len, testfile, testfile_len, corpus = data_generator(args)
+
+n_characters = len(corpus.dict)
+train_data = batchify(char_tensor(corpus, file), args.batch_size, args)
+val_data = batchify(char_tensor(corpus, valfile), 1, args)
+test_data = batchify(char_tensor(corpus, testfile), 1, args)
+print("Corpus size: ", n_characters)
+
+
+# num_chans = [args.nhid] * (args.levels - 1) + [args.emsize]
+
+print(args.cell)
+if args.cell == 'WaveGRU':
+    cell = WaveletGRU(input_size=args.emsize, out_size=n_characters, hidden_size=args.cell_size,
+                      mode=args.compression_mode)
+elif args.cell == 'GRU':
+    cell = GRUCell(input_size=args.emsize, out_size=n_characters, hidden_size=args.cell_size)
+else:
+    raise NotImplementedError()
+
+# model = TCN(args.emsize, n_characters, num_chans, kernel_size=k_size, dropout=dropout, emb_dropout=emb_dropout)
+
+
+class EmbeddingRnnWrapper(torch.nn.Module):
+    def __init__(self, cell, input_size, out_size):
+        super(EmbeddingRnnWrapper, self).__init__()
+        self.input_size = input_size
+        self.out_size = out_size
+        self.cell = cell
+        self.encoder = nn.Embedding(out_size, input_size)
+
+    def forward(self, x, h):
+        emb = self.encoder(x)
+        return self.cell(emb, h)
+
+
+model = EmbeddingRnnWrapper(cell, input_size=args.emsize, out_size=n_characters)
+
+if args.cuda:
+    model.cuda()
+
+criterion = nn.CrossEntropyLoss()
+lr = args.lr
+optimizer = getattr(optim, args.optim)(model.parameters(), lr=lr)
+
+
+def run_rnn(cell, input):
+    y_cell_lst = []
+    h = None
+    for t in range(input.shape[-1]):
+        # batch_major format [b,t,d]
+        y, h = cell(input[:, t], h)
+        y_cell_lst.append(y)
+
+    y = torch.stack(y_cell_lst, 1)
+    return y
+
+
+def evaluate(source):
+    model.eval()
+    total_loss = 0
+    count = 0
+    source_len = source.size(1)
+    with torch.no_grad():
+        for batch, i in enumerate(range(0, source_len - 1, args.validseqlen)):
+            if i + args.seq_len - args.validseqlen >= source_len:
+                continue
+            inp, target = get_batch(source, i, args)
+
+            # output = model(inp)
+            output = run_rnn(model, inp)
+
+            eff_history = args.seq_len - args.validseqlen
+            final_output = output[:, eff_history:].contiguous().view(-1, n_characters)
+            final_target = target[:, eff_history:].contiguous().view(-1)
+            loss = criterion(final_output, final_target)
+
+            total_loss += loss.data * final_output.size(0)
+            count += final_output.size(0)
+
+        val_loss = total_loss.item() / count * 1.0
+        return val_loss
+
+
+def train(epoch):
+    model.train()
+    total_loss = 0
+    start_time = time.time()
+    losses = []
+    source = train_data
+    source_len = source.size(1)
+    for batch_idx, i in enumerate(range(0, source_len - 1, args.validseqlen)):
+        if i + args.seq_len - args.validseqlen >= source_len:
+            continue
+        inp, target = get_batch(source, i, args)
+        optimizer.zero_grad()
+
+        # output = model(inp)
+        output = run_rnn(model, inp)
+
+        eff_history = args.seq_len - args.validseqlen
+        final_output = output[:, eff_history:].contiguous().view(-1, n_characters)
+        final_target = target[:, eff_history:].contiguous().view(-1)
+        loss = criterion(final_output, final_target)
+        loss.backward()
+
+        if args.clip > 0:
+            torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip)
+        optimizer.step()
+        total_loss += loss.item()
+
+        if batch_idx % args.log_interval == 0 and batch_idx > 0:
+            cur_loss = total_loss / args.log_interval
+            losses.append(cur_loss)
+            elapsed = time.time() - start_time
+            print('| epoch {:3d} | {:5d}/{:5d} batches | lr {:02.5f} | ms/batch {:5.2f} | '
+                  'loss {:5.3f} | bpc {:5.3f}'.format(
+                epoch, batch_idx, int((source_len-0.5) / args.validseqlen), lr,
+                              elapsed * 1000 / args.log_interval, cur_loss, cur_loss / math.log(2)))
+            total_loss = 0
+            start_time = time.time()
+
+    return sum(losses) * 1.0 / len(losses)
+
+
+def main():
+    global lr
+    try:
+        print("Training for %d epochs..." % args.epochs)
+        all_losses = []
+        best_vloss = 1e7
+        for epoch in range(1, args.epochs + 1):
+            loss = train(epoch)
+
+            vloss = evaluate(val_data)
+            print('-' * 89)
+            print('| End of epoch {:3d} | valid loss {:5.3f} | valid bpc {:8.3f}'.format(
+                epoch, vloss, vloss / math.log(2)))
+
+            test_loss = evaluate(test_data)
+            print('=' * 89)
+            print('| End of epoch {:3d} | test loss {:5.3f} | test bpc {:8.3f}'.format(
+                epoch, test_loss, test_loss / math.log(2)))
+            print('=' * 89)
+
+            if epoch > 5 and vloss > max(all_losses[-3:]):
+                lr = lr / 10.
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr
+            all_losses.append(vloss)
+
+            if vloss < best_vloss:
+                print("Saving...")
+                save(model)
+                best_vloss = vloss
+
+    except KeyboardInterrupt:
+        print('-' * 89)
+        print("Saving before quit...")
+        save(model)
+
+    # Run on test data.
+    test_loss = evaluate(test_data)
+    print('=' * 89)
+    print('| End of training | test loss {:5.3f} | test bpc {:8.3f}'.format(
+        test_loss, test_loss / math.log(2)))
+    print('=' * 89)
+
+# train_by_random_chunk()
+if __name__ == "__main__":
+    main()
