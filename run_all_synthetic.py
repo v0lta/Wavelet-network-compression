b3cf5580d7f6e308761c171d2e69f81b3ab10e2d
moritz <wolter@cs.uni-bonn.de>
2019-12-18 16:16:55 +0100

    memory problem accuracy factor fix. New runfile.

--- run_all_synthetic.py
+++ run_all_synthetic.py
@@ -7,11 +7,11 @@ subprocess.call('pwd')
 print('baseline GRU')
 time_str = str(datetime.datetime.today())
 print('adding problem baseline', time_str)
-with open("runs/baselineGRU_adding_" + time_str + ".txt", "w") as f:
+with open("runs/v2_baselineGRU_adding_" + time_str + ".txt", "w") as f:
     subprocess.call(['python', 'adding_memory_RNN_compression.py', '--problem', 'adding'], stdout=f)
 time_str = str(datetime.datetime.today())
 print('memory problem baseline', time_str)
-with open("runs/baselineGRU_memory_" + time_str + ".txt", "w") as f:
+with open("runs/v2_baselineGRU_memory_" + time_str + ".txt", "w") as f:
     subprocess.call(['python', 'adding_memory_RNN_compression.py', '--problem', 'memory'], stdout=f)
 
 experiment_lst = ['state', 'update', 'reset', 'state_reset', 'state_update', 'gates', 'full']
@@ -19,13 +19,13 @@ print('adding memory wavelet compression')
 for experiment in experiment_lst:
     time_str = str(datetime.datetime.today())
     print('adding ', experiment, ' at time:', time_str)
-    with open("runs/adding_" + experiment + time_str + ".txt", "w") as f:
+    with open("runs/v2_adding_" + experiment + time_str + ".txt", "w") as f:
         subprocess.call(['python', 'adding_memory_RNN_compression.py',
                          '--cell', 'WaveletGRU', '--compression_mode', experiment,
                          '--problem', 'adding'], stdout=f)
     time_str = str(datetime.datetime.today())
     print('memory ', experiment, ' at time:', time_str)
-    with open("runs/memory_" + experiment + time_str + ".txt", "w") as f:
+    with open("runs/v2_memory_" + experiment + time_str + ".txt", "w") as f:
         subprocess.call(['python', 'adding_memory_RNN_compression.py',
                          '--cell', 'WaveletGRU', '--compression_mode', experiment,
                          '--problem', 'memory'], stdout=f)
@@ -33,11 +33,11 @@ for experiment in experiment_lst:
 print('fastfood full compression')
 time_str = str(datetime.datetime.today())
 print('adding problem full compression', time_str)
-with open("runs/fastfood_adding_problem_full_compression_" + time_str + ".txt", "w") as f:
+with open("runs/v2_fastfood_adding_problem_full_compression_" + time_str + ".txt", "w") as f:
     subprocess.call(['python', 'adding_memory_RNN_compression.py', '--problem', 'adding',
                      '--cell', 'FastFoodGRU', '--compression_mode', 'full'], stdout=f)
 time_str = str(datetime.datetime.today())
 print('memory problem full compression', time_str)
-with open("runs/fastfood_memory_problem_full_compression_" + time_str + ".txt", "w") as f:
+with open("runs/v2_fastfood_memory_problem_full_compression_" + time_str + ".txt", "w") as f:
     subprocess.call(['python', 'adding_memory_RNN_compression.py', '--problem', 'memory',
                      '--cell', 'FastFoodGRU', '--compression_mode', 'full'], stdout=f)
