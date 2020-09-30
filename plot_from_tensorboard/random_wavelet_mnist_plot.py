f8f14e92c87ab63969f3381f0d3665821d4da64e
moritz wolter <moritz.wolter@scai.fraunhofer.de>
2020-09-30 14:45:53 +0200

    cleanup.

--- plot_from_tensorboard/random_wavelet_mnist_plot.py
+++
@@ -1,5 +0,0 @@
-# from tensorflow.python.summary import summary_iterator
-import tensorflow as tf
-
-event_file = '../runs/Oct07_14-25-41_infcuda_random_init_0_expl/events.out.tfevents.1570451141.infcuda.63767.3'
-
